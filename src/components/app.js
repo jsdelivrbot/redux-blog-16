52be69b7244491f7008a44a87021d19c72e1287e
Lucio Durán <lucio.duran@loi.com.uy>
2018-08-13 18:04:55 -0300

    starting

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,9 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export default class App extends Component {
   render() {
-    return <div>app</div>;
+    return (
+      <div>React simple starter</div>
+    );
   }
 }
